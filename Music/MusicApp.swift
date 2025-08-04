cda504acdf8332a045425d913afe70ba9ab2089a
Nayaki Maneeth Reddy <maneethreddy07@gmail.com>
2025-08-04 21:26:51 +0530

    Initial commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Maneeth Reddy on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
